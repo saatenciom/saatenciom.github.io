91917bef7e83d03b9ee14dfdc6a7515f71f2beb3
saatenciom <saatenciom@gmail.com>
2025-10-25 13:38:46 -0500

    Update metadata for Homework 2

--- HW2.js
+++ HW2.js
@@ -1,10 +1,10 @@
  /*
     Name: Simon Atencio Machado
     File: Homework1.js
-    Date created: September 26, 2025
-    Date last edit: September 26, 2025
+    Date created: October 24, 2025
+    Date last edit: October 26, 2025
     Version: 1
-    Description: JavaScript for Homework 1 for MIS 3371
+    Description: JavaScript for Homework 2 for MIS 3371
 */
 //dynamic date js code
 const d = new Date();
