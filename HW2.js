a8a0dfb0a6806deb622f9447d360ced9c0ea0a9a
saatenciom <saatenciom@gmail.com>
2025-10-26 13:56:57 -0500

    Implement DOB and SSN validation functions
    Added validation functions for date of birth and SSN.

--- HW2.js
+++ HW2.js
@@ -11,3 +11,51 @@ const d = new Date();
 let text = d.toLocaleDateString();
 document.getElementById("today").innerHTML = text;
 
+function validatedob() 
+{
+    dob = document.getElementById("DOB");
+    let date = new Date(DOB.value);
+    let maxDate = new Date().setFullYear(new Date().getFullYear() - 120);
+
+    if (date > new Date()) 
+    {
+        document.getElementById("DOBerror").innerHTML = 
+        "Date can't be in the future";
+        dob.value = "";
+        return false;
+    } 
+      else 
+         if (date < new Date(maxDate)) 
+         {
+        document.getElementById("DOBerror").innerHTML = 
+        "Date can't be more than 120 years ago";
+        dob.value = "";
+        return false;
+         } 
+         else 
+         {
+        document.getElementById("DOBerror").innerHTML = "";
+        return true;
+        }
+}
+
+function validateSSN()
+{
+      const ssn = document.getElementById("SSN").value;
+      const ssnR = /^[0-9]{3}-?[0-9]{2}-?[0-9]{4}$/;
+
+    if (!ssnR.test(ssn)) 
+    {
+        document.getElementById("SSNerror").innerHTML = 
+        "Please enter a valid SSN";
+        return false;
+    } 
+    else 
+    {
+        document.getElementById("SSNerror").innerHTML = "";
+        return true;
+    }
+
+ 
+}
+
